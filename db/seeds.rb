73e648bdaf646b7c12353497f6b8ae050dcd7d2e
pohor <ola.pohorecka@gmail.com>
2019-01-03 17:27:39 +0100

    Amended seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,48 +6,66 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Genre.new(name: 'action').save
-Genre.new(name: 'adventure').save
-Genre.new(name: 'animation').save
-Genre.new(name: 'biography').save
-Genre.new(name: 'comedy').save
-Genre.new(name: 'crime').save
-Genre.new(name: 'documentary').save
-Genre.new(name: 'drama').save
-Genre.new(name: 'family').save
-Genre.new(name: 'fantasy').save
-Genre.new(name: 'history').save
-Genre.new(name: 'horror').save
-Genre.new(name: 'musical').save
-Genre.new(name: 'romance').save
-Genre.new(name: 'scifi').save
-Genre.new(name: 'thriller').save
-Genre.new(name: 'war').save
-Genre.new(name: 'western').save
+#!!!Please use only after the first user is created!!!
 
-Director.new(name: 'Steven Spilberg').save
-Director.new(name: 'Alfred Hitckcock').save
-Director.new(name: 'Martin Scorsese').save
-Director.new(name: 'Ridley Scott').save
-Director.new(name: 'Quentin Tarantino').save
-Director.new(name: 'Francis Ford Copolla').save
-Director.new(name: 'Stanley Kubrick').save
-Director.new(name: 'George Lucas').save
-Director.new(name: 'Woody Allen').save
-Director.new(name: 'Mel Brooks').save
-Director.new(name: 'Mel Gibson').save
-Director.new(name: 'Daren Aronofsky').save
-Director.new(name: 'Wes Anderson').save
-Director.new(name: 'Peter Jackson').save
+#comment out the below if you don't want the first user to be admin.
 
-Screenwriter.new(name: 'Quentin Tarantino').save
-Screenwriter.new(name: 'Woody Allen').save
-Screenwriter.new(name: 'Billy Wilder').save
-Screenwriter.new(name: 'Oliver Stone').save
-Screenwriter.new(name: 'Francis Ford Copolla').save
-Screenwriter.new(name: 'Christopher Nolan').save
-Screenwriter.new(name: 'George Lucas').save
-Screenwriter.new(name: 'Coen Brothers').save
-Screenwriter.new(name: 'Charlie Kaufma').save
-Screenwriter.new(name: 'Nora Ephron').save
-Screenwriter.new(name: 'James Cameron').save
+User.first.update_attributes(admin: true)
+
+Genre.new(name: 'action', user: User.first).save
+Genre.new(name: 'adventure', user: User.first).save
+Genre.new(name: 'animation', user: User.first).save
+Genre.new(name: 'biography', user: User.first).save
+Genre.new(name: 'comedy', user: User.first).save
+Genre.new(name: 'crime', user: User.first).save
+Genre.new(name: 'documentary', user: User.first).save
+Genre.new(name: 'drama', user: User.first).save
+Genre.new(name: 'family', user: User.first).save
+Genre.new(name: 'fantasy', user: User.first).save
+Genre.new(name: 'history', user: User.first).save
+Genre.new(name: 'horror', user: User.first).save
+Genre.new(name: 'musical', user: User.first).save
+Genre.new(name: 'romance', user: User.first).save
+Genre.new(name: 'scifi', user: User.first).save
+Genre.new(name: 'thriller', user: User.first).save
+Genre.new(name: 'war', user: User.first).save
+Genre.new(name: 'western', user: User.first).save
+
+Director.new(name: 'Steven Spilberg', user: User.first).save
+Director.new(name: 'Alfred Hitckcock', user: User.first).save
+Director.new(name: 'Martin Scorsese', user: User.first).save
+Director.new(name: 'Ridley Scott', user: User.first).save
+Director.new(name: 'Quentin Tarantino', user: User.first).save
+Director.new(name: 'Francis Ford Copolla', user: User.first).save
+Director.new(name: 'Stanley Kubrick', user: User.first).save
+Director.new(name: 'George Lucas', user: User.first).save
+Director.new(name: 'Woody Allen', user: User.first).save
+Director.new(name: 'Mel Brooks', user: User.first).save
+Director.new(name: 'Mel Gibson', user: User.first).save
+Director.new(name: 'Daren Aronofsky', user: User.first).save
+Director.new(name: 'Wes Anderson', user: User.first).save
+Director.new(name: 'Peter Jackson', user: User.first).save
+
+Screenwriter.new(name: 'Quentin Tarantino', user: User.first).save
+Screenwriter.new(name: 'Woody Allen', user: User.first).save
+Screenwriter.new(name: 'Billy Wilder', user: User.first).save
+Screenwriter.new(name: 'Oliver Stone', user: User.first).save
+Screenwriter.new(name: 'Francis Ford Copolla', user: User.first).save
+Screenwriter.new(name: 'Christopher Nolan', user: User.first).save
+Screenwriter.new(name: 'George Lucas', user: User.first).save
+Screenwriter.new(name: 'Coen Brothers', user: User.first).save
+Screenwriter.new(name: 'Charlie Kaufma', user: User.first).save
+Screenwriter.new(name: 'Nora Ephron', user: User.first).save
+Screenwriter.new(name: 'James Cameron', user: User.first).save
+
+Origin.new(name: 'Poland', description: "There are no mistakes. You can fix anything that happens. Every single thing in the world has its own personality - and it is up to you to make friends with the little rascals. Follow the lay of the land. It's most important.").save
+Origin.new(name: 'USA', description: "There are no mistakes. You can fix anything that happens. Every single thing in the world has its own personality - and it is up to you to make friends with the little rascals. Follow the lay of the land. It's most important.").save
+Origin.new(name: 'Spain', description: "There are no mistakes. You can fix anything that happens. Every single thing in the world has its own personality - and it is up to you to make friends with the little rascals. Follow the lay of the land. It's most important.").save
+Origin.new(name: 'Italy', description: "There are no mistakes. You can fix anything that happens. Every single thing in the world has its own personality - and it is up to you to make friends with the little rascals. Follow the lay of the land. It's most important.").save
+Origin.new(name: 'Great Britain', description: "There are no mistakes. You can fix anything that happens. Every single thing in the world has its own personality - and it is up to you to make friends with the little rascals. Follow the lay of the land. It's most important.").save
+Origin.new(name: 'Japan', description: "There are no mistakes. You can fix anything that happens. Every single thing in the world has its own personality - and it is up to you to make friends with the little rascals. Follow the lay of the land. It's most important.").save
+Origin.new(name: 'Turkey', description: "There are no mistakes. You can fix anything that happens. Every single thing in the world has its own personality - and it is up to you to make friends with the little rascals. Follow the lay of the land. It's most important.").save
+Origin.new(name: 'France', description: "There are no mistakes. You can fix anything that happens. Every single thing in the world has its own personality - and it is up to you to make friends with the little rascals. Follow the lay of the land. It's most important.").save
+Origin.new(name: 'Sweden', description: "There are no mistakes. You can fix anything that happens. Every single thing in the world has its own personality - and it is up to you to make friends with the little rascals. Follow the lay of the land. It's most important.").save
+Origin.new(name: 'Iran', description: "There are no mistakes. You can fix anything that happens. Every single thing in the world has its own personality - and it is up to you to make friends with the little rascals. Follow the lay of the land. It's most important.").save
+Origin.new(name: 'Germany', description: "There are no mistakes. You can fix anything that happens. Every single thing in the world has its own personality - and it is up to you to make friends with the little rascals. Follow the lay of the land. It's most important.").save

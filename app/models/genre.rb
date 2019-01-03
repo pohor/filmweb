e10a5b03bdd4264dc5c0921489d565fc76f6ed29
pohor <ola.pohorecka@gmail.com>
2019-01-03 18:39:51 +0100

    Add devise views and format

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,7 +2,7 @@ class Genre < ApplicationRecord
   validates :name, presence: true, length: 2..50
 
   has_many :genres_films
-  has_many :films, through: :categories_products
+  has_many :films, through: :genres_films
 
   belongs_to :user
 end
--- app/models/origin.rb
+++ app/models/origin.rb
@@ -3,6 +3,6 @@ class Origin < ApplicationRecord
   validates :description, presence: true, length: 3..500
 
   has_many :origins_films
-  has_many :films, through: :origins_products
+  has_many :films, through: :origins_films
 
 end

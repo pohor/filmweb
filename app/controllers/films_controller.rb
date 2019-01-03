6347d61565043b440194b2ed6d0ac57d039c7eae
pohor <ola.pohorecka@gmail.com>
2019-01-03 13:49:48 +0100

    Corrected views for films and amedned form for film to pass params

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -80,6 +80,6 @@ class FilmsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def film_params
-      params.require(:film).permit(:title, :premiere, :duration, genre_ids: [], director_ids: [], screenwriters_ids: [])
+      params.require(:film).permit(:title, :premiere, :duration, genre_ids: [], director_ids: [], screenwriter_ids: [])
     end
 end
